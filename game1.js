8c44775a7ab1f50a6d035f282ca45bc0c6adc81c
Kbhral <re3c1aoli@gmail.com>
2023-10-05 03:01:10 -0400

    initial commit

---
+++ game1.js
@@ -0,0 +1,31 @@
+//Definição da função que calcula o dano em combate
+function calcularDano(tipoItem, dano) {
+    if (tipoItem.toLowerCase() === 'arma') {
+      return dano * 2;  // Dano dobrado para arma
+    } else {
+      return dano;  // Dano normal para outros tipos de item
+    }
+  }
+  
+  // Pedir ao usuário para inserir os detalhes do item mágico
+  const tipoItem = "Espada Sagrada";
+  const dano = "5000 dano fisico";
+  const armadura = "100";
+  
+  // Criar o objeto que representa o item mágico
+  const itemMagico = {
+    tipo: tipoItem,
+    dano: dano,
+    armadura: armadura,
+  };
+  
+  // Calcular o dano em combate
+  const danoEmCombate = calcularDano(itemMagico.tipo, itemMagico.dano);
+  
+  // Imprimir os detalhes do item mágico e o dano em combate
+  console.log('Detalhes do item mágico:');
+  console.log('Tipo: ' + itemMagico.tipo);
+  console.log('Dano: ' + itemMagico.dano);
+  console.log('Armadura: ' + itemMagico.armadura);
+  console.log('Dano em combate: ' + danoEmCombate);
+  
